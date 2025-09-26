3765ad833cbbfef946dba51a6869492c685b1390
ASiddiqui2614 <1543507@learn.vsb.bc.ca>
2025-09-26 21:41:54 +0000

    first line

---
+++ 2.4/main.py
@@ -0,0 +1,14 @@
+file = open("2.4/responses.csv")
+
+# junk = file.readline()
+# data = file.readline()
+
+# print(data)
+# datalist = data.split(",")
+# print(datalist)
+
+
+
+for person in file:
+    if "ashar" in line.lower():
+        print(line)
