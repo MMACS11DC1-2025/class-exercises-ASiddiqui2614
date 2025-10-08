b3c52c3695745f254561d2d71ad43db89d49c027
ASiddiqui2614 <1543507@learn.vsb.bc.ca>
2025-10-08 21:42:01 +0000

    finished comparing

--- 2.5/comparing.py
+++ 2.5/comparing.py
@@ -9,13 +9,18 @@ Test as you go! Describe in your comments what steps you took to test your code.
 file = open("2.4/responses.csv")
 lines = []
 ppl = []
+confirm = True
 nameid = 0
 for line in file:
     lines.append(line.lower().split(","))
 
-
-answer = input("What is your full name? \n").lower().strip()
-
+while confirm:
+    answer = input("What is your full name? \n").lower().strip()
+    for person in lines:
+        if answer == person[1].lower():
+            confirm = False
+    if confirm == True:
+        print("Sorry, please try again")
 #finds user id
 for person in range(len(lines)):
     if answer == lines[person][1].lower():
@@ -23,20 +28,18 @@ for person in range(len(lines)):
 
 #finds whoever is similar to user based on their choice of pet and music preferance
 #person is other person. here, we are checking to make sure it's not comparing to user
+simiAmount = 0
 for person in range(len(lines)):
     if person != nameid:
         simiAmount = 0
         for i in [3, 7]:
             if lines[nameid][i] == lines[person][i]:
                 simiAmount += 1
-        # print(person)
-        # print(simiAmount)
         if simiAmount == 2:
             ppl.append(int(lines[person][0]))
-
-
 #output
 for person in ppl:
-    print("You are most similar to " + lines[person][1] + " in terms of your favorite animal and music choice")
+    if simiAmount == 0:    
+        print("You are most similar to " + lines[person][1] + " in terms of your favorite animal and music choice")
     
 
