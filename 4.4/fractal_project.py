be1ff51ed9201d0351702ee92cf486ff3e1ab4eb
ASiddiqui2614 <1543507@learn.vsb.bc.ca>
2025-10-28 21:59:43 +0000

    added fractal, add recursion tmrw

--- 4.4/fractal_project.py
+++ 4.4/fractal_project.py
@@ -1,10 +1,23 @@
 import turtle
 t = turtle.Turtle()
+s = turtle.Screen()
+colors = ['#29C7BD', '#1487B0', '#14B06C', '#10BA2D', '#45DB21', '#FFFFFF']
+s.bgcolor('black')
+t.speed('fastest')
+t.hideturtle()
 
-def spiral(x, y):
-  while x > 0:
-    t.right(10 + x)
-    t.forward(10 + y)
-    
-    x -= 1
-spiral(30, 10)
+# Infinite loop to continuously draw and erase spiral pattern
+
+while True:
+  for x in range(200):
+    t.pencolor(colors[x % len(colors)])
+    t.width(x / 300 + 3)
+    t.forward(x)
+    t.left(59)
+  t.right(239)
+  
+  for x in range(200, 0, -3):
+    t.pencolor('black')
+    t.width(x / 100 + 10)
+    t.forward(x)
+    t.right(59)
