c4f20d36f3e9a7e2e2c59295110697bbefd4c0da
ASiddiqui2614 <1543507@learn.vsb.bc.ca>
2025-10-29 00:40:55 +0000

    finished fractal/recursion thingy

--- 4.4/fractal_project.py
+++ 4.4/fractal_project.py
@@ -6,9 +6,12 @@ s.bgcolor('black')
 t.speed('fastest')
 t.hideturtle()
 
-# Infinite loop to continuously draw and erase spiral pattern
-
-while True:
+# creates a certain amount of semi erased spirals that looks pretty cool, amt of times is based on user input
+count = int(input("How many times do you want to repeat the spiral? "))
+def draw_spiral(count):
+  if count == 0:
+    return
+    
   for x in range(200):
     t.pencolor(colors[x % len(colors)])
     t.width(x / 300 + 3)
@@ -21,3 +24,9 @@ while True:
     t.width(x / 100 + 10)
     t.forward(x)
     t.right(59)
+    
+  draw_spiral(count - 1)
+  
+draw_spiral(count)
+turtle.done()
+    
