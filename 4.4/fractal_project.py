4f0b153db235497e0370395cf8a673646b61d3f7
ASiddiqui2614 <1543507@learn.vsb.bc.ca>
2025-10-31 21:08:53 +0000

    nearly finished fractal thingy

--- 4.4/fractal_project.py
+++ 4.4/fractal_project.py
@@ -1,7 +1,10 @@
 import turtle
 t = turtle.Turtle()
 s = turtle.Screen()
-colors = ['#29C7BD', '#1487B0', '#14B06C', '#10BA2D', '#45DB21', '#FFFFFF']
+colorsbg = ['#29C7BD', '#1487B0', '#14B06C', '#10BA2D', '#45DB21', '#FFFFFF']
+colorsrb = ['#FE0002', '#D80027', '#A1015D', '#63009E', '#2A00d5', '#0302FC']
+colorsrg = ['#00FF00', '#95FF66', '#C8FFb3', '#FFC2AC', '#FF6B5E', '#FF0000']
+colors_dict = {"bg":colorsbg, "rb":colorsrb, "rg":colorsrg}
 s.bgcolor('black')
 t.speed('fastest')
 t.hideturtle()
@@ -9,15 +12,17 @@ t.hideturtle()
 # creates a certain amount of semi erased spirals that looks pretty cool, amt of times is based on user input
 count = int(input("How many times do you want to repeat the spiral? "))
 print("Function has been repeated " + str(count) + " times.")
+c = input("What color do you want it to be? bg, rg, or rg. Say exactly like that: ")
 def draw_spiral(count):
   if count == 0:
     return
-    
+  
+  colors = colors_dict[c]
   for x in range(200):
-    t.pencolor(colors[x % len(colors)])
-    t.width(x / 300 + 3)
-    t.forward(x)
-    t.left(59)
+      t.pencolor(colors[x % len(colors)])
+      t.width(x / 300 + 3)
+      t.forward(x)
+      t.left(59)
   t.right(239)
   
   for x in range(200, 0, -3):
