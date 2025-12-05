d5d1e8c0201ddc8db565d16b622a3f50bb5c1698
ASiddiqui2614 <1543507@learn.vsb.bc.ca>
2025-12-05 21:12:08 +0000

    gk

--- 6.7/green_finder.py
+++ 6.7/green_finder.py
@@ -8,72 +8,81 @@ counter = 0
 
 pics = []
 
+tree = "6.7/tree.png"
+pics.append(tree)
+
+bliss = "6.7/bliss.png"
+pics.append(bliss)
+
+pepper = "6.7/bellpepper.png"
+pics.append(pepper)
+
+parrot = "6.7/parrot.png"
+pics.append(parrot)
+
 def colour(r, g, b):
-    if  5 <= r < 202 and 20 <= 217 and 0 <= b <= 109:
-        return "green"
+        if  51.5 <= r <= 89 and 72.5 <= g <= 217 and 30.5 <= b <= 80.5:
+            return "green"
 
-    
-   
+for file in pics:
+    file1 = Image.open(file)
+    jbImage = file1.load()
 
-file = Image.open("6.7/tree.png")
-jbImage = file.load()
+    t1 = time.time()
 
+    width = file1.width
+    height = file1.height
 
-
-t1 = time.time()
-
-
-greenPixels = []
+    t1 = time.time()
 
 
-width = file.width
-height = file.height
+    greenPixels = []
 
-for x in range(width):
-    for y in range(height):
-        pixel_r = jbImage[x, y][0]
-        pixel_g = jbImage[x, y][1]
-        pixel_b = jbImage[x, y][2]
+    for x in range(width):
+        for y in range(height):
+            pixel_r = jbImage[x, y][0]
+            pixel_g = jbImage[x, y][1]
+            pixel_b = jbImage[x, y][2]
 
-        if colour(pixel_r, pixel_g, pixel_b) == "green":
-            greenPixels.append(jbImage[x, y])
-            counter += 1
+            if colour(pixel_r, pixel_g, pixel_b) == "green":
+                greenPixels.append(jbImage[x, y])
+                counter += 1
 
 
-t2 = time.time()
+    t2 = time.time()
 
-numGreen = len(greenPixels)
+    numGreen = len(greenPixels)
 
-totalPixels = width*height
-greenRatio = numGreen / totalPixels
+    totalPixels = width*height
+    greenRatio = numGreen / totalPixels
 
-greenPercent = greenRatio * 100
+    greenPercent = greenRatio * 100
 
-t3 = time.time()
+    t3 = time.time()
 
 
 
-module_load = t1 - t0
-image_open_load = t2 - t1
-loop = t3-t2
-entire = t3 - t0
+    module_load = t1 - t0
+    image_open_load = t2 - t1
+    loop = t3-t2
+    entire = t3 - t0
 
-rating = (counter / totalPixels) * 100
+    rating = (counter / totalPixels) * 100
 
-if rating < 10:
-    print("This picture is bad, not enough green, it makes me really mad >>:(")
-elif rating < 20:
-    print("This picture is not bad, but not enough green, it makes me mad >:(")
-elif rating < 50:
-    print("This picture is mid, not enough green, but it's better than nothing :|")
-elif rating < 75:
-    print("This picture is good, a good amount of green, it makes happy :)")
-elif rating < 90:
-    print("This picture is soooo good, enough green to last for a long time, it makes me very happy :)))")
-elif rating <= 100:
-    print("This is too much green, why do you need so much")
+    if rating < 10:
+        print("This picture is bad, not enough green, it makes me really mad >>:(")
+    elif rating < 20:
+        print("This picture is not bad, but not enough green, it makes me mad >:(")
+    elif rating < 50:
+        print("This picture is mid, not enough green, but it's better than nothing :|")
+    elif rating < 75:
+        print("This picture is good, a good amount of green, it makes happy :)")
+    elif rating < 90:
+        print("This picture is soooo good, enough green to last for a long time, it makes me very happy :)))")
+    elif rating <= 100:
+        print("This is too much green, why do you need so much")
 
-timings = "It took {:.2f}s to import the PIL, {:.2f}s to load the image, and {:.2f}s to do the loop. All in all it took {:.2f}s.".format(module_load, image_open_load, loop, entire)
-print(timings)
-perc = "{:.3f}% of the picture is green".format(rating)
-print(perc)
+    timings = "It took {:.2f}s to import the PIL, {:.2f}s to load the image, and {:.2f}s to do the loop. All in all it took {:.2f}s.".format(module_load, image_open_load, loop, entire)
+    print(timings)
+    perc = "{:.3f}% of the picture is green".format(rating)
+    print(perc)
