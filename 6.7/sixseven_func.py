f76469b6a452bcbb4e7924f7c9f5ed4176e14ed0
ASiddiqui2614 <1543507@learn.vsb.bc.ca>
2025-12-15 21:09:47 +0000

    working on search function, will need help

--- 6.7/sixseven_func.py
+++ 6.7/sixseven_func.py
@@ -2,7 +2,8 @@ def colour(r, g, b):
         if  (1 <= r <= 238 and 54 <= g <= 255 and 1 <= b <= 238):
             return "green"
         
-def binary(listname, x):
+def search(listname):
       first = 0
       last = len(listname) - 1
-      while 
+      while first <= last:
+            
