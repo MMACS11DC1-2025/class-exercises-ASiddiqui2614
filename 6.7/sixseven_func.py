98428a8db163ebdb95d33b8c3b0010f6f148f61c
ASiddiqui2614 <1543507@learn.vsb.bc.ca>
2025-12-18 21:55:13 +0000

    made solid idea for search function

--- 6.7/sixseven_func.py
+++ 6.7/sixseven_func.py
@@ -2,8 +2,13 @@ def colour(r, g, b):
         if  (1 <= r <= 238 and 54 <= g <= 255 and 1 <= b <= 238):
             return "green"
         
-def search(listname):
+def search(listname, min, max):
       first = 0
       last = len(listname) - 1
+      
       while first <= last:
+            mid =int((first+last)/2)
+
+            if min <= listname[mid] <= max:
+                  return listname[mid]
             
